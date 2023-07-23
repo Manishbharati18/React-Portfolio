595b254206017db9aa53ffc658448fd7b5a6e46b
RunTime Error <110253640+Manishbharati18@users.noreply.github.com>
2023-07-23 12:01:43 +0530

    Update Projects.js

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -4,8 +4,8 @@ import {motion} from "framer-motion";
 
 import Featured from '../components/Featured';
 import Archive from '../components/Archive';
-import studiebnb from "../assets/videoes/studiebnb.mp4"
-import ecommerce from "../assets/videoes/ecommerce.mp4"
+// import studiebnb from "../assets/videoes/studiebnb.mp4"
+// import ecommerce from "../assets/videoes/ecommerce.mp4"
 
 const technologies1 = [
   "SQL",
@@ -27,13 +27,13 @@ function Projects() {
         <Header><span>02.</span> PROJECTS</Header>
         <FeaturedContainer>
           <Featured 
-            video={ecommerce} 
+            // video={ecommerce} 
             title="Sneaker Ecommerce"
             text="This e-commerce application allowed me to build on my backend knowledge from school and combine it with my self-taught front-end skills. It provides customers with the ability to log in, view, and purchase sneakers, while administrators have access to features for managing products, orders, and users. "
             link="https://github.com/mariangle/ecommerce-app-ms-sql-net-react"
             technologies={technologies1} />
           <Featured 
-            video={studiebnb} 
+            // video={studiebnb} 
             title="StudieBnB"
             text="StudieBnb  offers users the ability to log in, book listings, and manage their own listings. The project was developed to explore the MERN stack and writing server-side code in JavaScript"
             link="https://github.com/mariangle/mern-booking-app"
